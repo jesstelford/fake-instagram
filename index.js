0b90050c81562251f0d56729ea31304bd9306d6e
Jess Telford <jess+github@jes.st>
2016-06-29 09:10:00 +1000

    More fake api setup

--- index.js
+++ index.js
@@ -1,57 +1,104 @@
+var qs = require('qs');
+var cuid = require('cuid');
+var pick = require('lodash').pick;
 var restify = require('restify');
 var authorizeTemplate = require('./templates/authorize');
 
 function isMissingQueries(req, queries) {
-  return queryies.some(function(requiredQuery) {
+  return queries.some(function(requiredQuery) {
     if (!req.query[requiredQuery]) {
       return true;
     }
   });
 }
 
+var cache = {}
+
+function getAccessToken(clientId) {
+  cache[clientId] = cache[clientId] || {}
+  cache[clientId].accessTokens = cache[clientId].accessTokens || []
+  if (!cache[clientId].accessTokens.length) {
+    cache[clientId].accessTokens.push(createAccessToken(clientId))
+  }
+  return cache[clientId].accessTokens[0]
+}
+
+function getCode(clientId) {
+  cache[clientId] = cache[clientId] || {}
+  cache[clientId].codes = cache[clientId].codes || []
+  if (!cache[clientId].codes.length) {
+    cache[clientId].codes.push(createCode(clientId))
+  }
+  return cache[clientId].codes[0]
+}
+
+function createAccessToken(clientId) {
+  return cuid()
+}
+
+function createCode(clientId) {
+  return cuid()
+}
+
 
 var server = restify.createServer();
 
 server.pre(restify.pre.userAgentConnection());
+server.pre(restify.pre.sanitizePath());
 server.use(restify.queryParser());
 
 // User allowed app authorization
-server.post('/oauth/authorize/success', function respond(req, res, next) {
+server.get('/oauth/authorize/success', function respond(req, res, next) {
 
-  if (isMissingQueries(req, ['redirect_uri'])) {
+  var requiredQueries = ['client_id', 'redirect_uri', 'response_type']
+
+  if (isMissingQueries(req, requiredQueries)) {
     // TODO: error
   }
 
-  // TODO: Generate an access token
-  var accessToken = 'ABC123';
+  var queryResult = {};
 
-  // http://your-redirect-uri#access_token=ACCESS-TOKEN
-  res.redirect(302, `${req.query['redirect_uri']}#access_token=${accessToken}`, next);
+  if (req.query.response_type === 'token') {
+    // Generate an access token
+    queryResult.access_token = getAccessToken(req.query.client_id)
+  } else if(req.query.response_type === 'code') {
+    // Generate a 'code' (which wont work for API requests)
+    queryResult.code = getCode(req.query.client_id)
+  }
+
+  console.log('[cache]', cache);
+
+  // eg; http://your-redirect-uri#access_token=ACCESS-TOKEN
+  res.redirect(302, `${req.query.redirect_uri}#${qs.stringify(queryResult)}`, next);
 
 })
 
 // User denied app authorization
-server.post('/oauth/authorize/denied', function respond(req, res, next) {
+server.get('/oauth/authorize/denied', function respond(req, res, next) {
 
   if (isMissingQueries(req, ['redirect_uri'])) {
     // TODO: error
   }
 
-  res.redirect(302, `${req.query['redirect_uri']}?error=access_denied&error_reason=user_denied&error_description=The+user+denied+your+request`, next);
+  res.redirect(302, `${req.query.redirect_uri}?error=access_denied&error_reason=user_denied&error_description=The+user+denied+your+request`, next);
 
 })
 
 // The Authorize step shown to the user
 // /oauth/authorize/?client_id=CLIENT-ID&redirect_uri=REDIRECT-URI&response_type=token
-server.post('/oauth/authorize/', function respond(req, res, next) {
+server.get('/oauth/authorize/', function respond(req, res, next) {
 
-  if (isMissingQueries(req, ['client_id', 'redirect_uri', 'response_type'])) {
+  var requiredQueries = ['client_id', 'redirect_uri', 'response_type']
+
+  if (isMissingQueries(req, requiredQueries)) {
     // TODO: error
   }
 
-  res.send(authorizeTemplate({
-    authorizeUrl: `/oauth/authorize/success?redirect_uri=${req.query['redirect_uri']}`,
-    noAuthorizeUrl: `/oauth/authorize/denied?redirect_uri=${req.query['redirect_uri']}`
+  var forwardQueries = qs.stringify(pick(req.query, requiredQueries))
+
+  res.end(authorizeTemplate({
+    authorizeUrl: `/oauth/authorize/success?${forwardQueries}`,
+    noAuthorizeUrl: `/oauth/authorize/denied?${forwardQueries}`
   }))
 
   next();
